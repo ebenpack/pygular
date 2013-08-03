ebenpack/pygular

commit e46c4ffea1012df915d21499a7a16e52ca4c7ed3

Removed replacement of \n with <br>. Not necessary when text returned is wrapped in <pre>

app/pygular.py
@@ -56,7 +56,7 @@ def capture_groups(regexp, text):
                     if groupindex and (j+1) in groupindex:
                         newmatch.append({groupindex[(j+1)]: match})
                     else:
-                        #If match is None (which is returned the match object group method when a group is contained
+                        # If match is None (which is returned by the match object group() method when a group is contained
                         # in a part of the pattern that did not match), just add an empty string to the match group.
                         # Alternatively, something like "<span class='alert'>No Match</span>" could be returned, but
                         # I'm not sure if that's appropriate/correct or not.
@@ -99,8 +99,6 @@ def regexp_highlight(regexp, text, html_class="match hilite"):
             if i == span_list[-1][1]:
                 newtext.append('</span>')
                 span_list.pop()
-        if c == '\n':
-            newtext.append("<br />")
         else:
             newtext.append(cgi.escape(c))
     return "".join(newtext)

commit d7a885f39b70144d27dcddaa5a980131b7175277

Fixed highlight bug introduced in commit e46c4ffea1 (removed if statement, but not else)

app/pygular.py
@@ -99,8 +99,7 @@ def regexp_highlight(regexp, text, html_class="match hilite"):
             if i == span_list[-1][1]:
                 newtext.append('</span>')
                 span_list.pop()
-        else:
-            newtext.append(cgi.escape(c))
+        newtext.append(cgi.escape(c))
     return "".join(newtext)
 
 